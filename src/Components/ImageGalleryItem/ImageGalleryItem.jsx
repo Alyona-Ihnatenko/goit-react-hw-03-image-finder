e97028771558199a46042bc83de8467fae462715
Alyona Ihnatenko <altaig4@gmail.com>
2021-09-04 13:54:08 +0300

    add hw

---
+++ src/Components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -0,0 +1,28 @@
+
+import PropTypes from 'prop-types';
+import css from '../ImageGalleryItem/ImageGalleryItem.module.css';
+
+
+function ImageGalleryItem(props) {
+  const { webformatURL, tags } = props.hit;
+  const { showModal } = props;
+
+  return (
+    <li className={css.item}>
+      <img
+        width="600"
+        src={webformatURL}
+        alt={tags}
+        className={css.ImageGalleryItemImage}
+        onClick={() => showModal(props.hit)}
+      />
+    </li>
+  );
+}
+
+ImageGalleryItem.propTypes = {
+  hit: PropTypes.object,
+  showModal: PropTypes.func,
+};
+
+export  {ImageGalleryItem};
---
+++ src/Components/Searchbar/Searchbar.jsx
@@ -0,0 +1,55 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import { toast } from 'react-toastify';
+import css from './Searchbar.module.css';
+
+class Searchbar extends Component {
+  state = {
+    query: '',
+  };
+
+  handleChange = e => {
+    this.setState({ query: e.currentTarget.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    if (this.state.query.trim() === '') {
+      return toast.warn('Empty request - please input a query');
+    }
+    this.props.onSubmit(this.state.query);
+    this.reset();
+  };
+
+  reset = () => {
+    this.setState({ query: '' });
+  };
+
+  render() {
+    return (
+      <header className={css.Searchbar}>
+        <form className={css.SearchForm} onSubmit={this.handleSubmit}>
+          <button type="submit" className={css.SearchFormButton}>
+            <span className={css.SearchFormButtonLabel}>Search</span>
+          </button>
+
+          <input
+            className={css.SearchFormInput}
+            type="text"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+            value={this.state.query}
+            onChange={this.handleChange}
+          />
+        </form>
+      </header>
+    );
+  }
+}
+
+Searchbar.propTypes = {
+  onSubmit: PropTypes.func,
+};
+
+export  {Searchbar};
